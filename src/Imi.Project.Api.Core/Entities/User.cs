5d2f460aa9487cd161fea440e353d6b689144248
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:34:53 +0100

    Create User entity

---
+++ src/Imi.Project.Api.Core/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public bool IsAdmin { get; set; }
+        public ICollection<Character> Characters { get; set; }
+    }
+}
