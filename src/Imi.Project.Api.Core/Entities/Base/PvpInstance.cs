145468103229814e981fe9da4a5f7a78250b914d
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:33:02 +0100

    Create PvpInstance base class

---
+++ src/Imi.Project.Api.Core/Entities/Base/PvpInstance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Base
+{
+    public abstract class PvpInstance
+    {
+        public bool IsWon { get; set; }
+        public DateTime Date { get; set; }
+        public Guid LeaderId { get; set; }
+        public Character Leader { get; set; }
+        public ICollection<Character> CharactersParticipated { get; set; }
+    }
+}
