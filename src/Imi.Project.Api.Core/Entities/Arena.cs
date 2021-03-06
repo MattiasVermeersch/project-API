fc98fbd321f37a921be7f10ea34b3330648c3db3
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:36:29 +0100

    Create Arena entity

---
+++ src/Imi.Project.Api.Core/Entities/Arena.cs
@@ -0,0 +1,12 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class Arena : PvpInstance
+    {
+        public ICollection<ArenaCharacter> ArenaCharacters { get; set; }
+    }
+}
