4146410279a91f3e47d95c169a8c6d7f41343323
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:36:38 +0100

    Create Battleground entity

---
+++ src/Imi.Project.Api.Core/Entities/Battleground.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class Battleground : PvpInstance
+    {
+        public string InstanceName { get; set; }
+        public ICollection<BattlegroundCharacter> BattlegroundCharacters { get; set; }
+    }
+}
