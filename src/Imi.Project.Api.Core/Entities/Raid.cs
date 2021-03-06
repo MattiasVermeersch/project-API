12d2bb805b2517cdd4921d00716e77c581dc2333
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:37:17 +0100

    Create Raid entity

---
+++ src/Imi.Project.Api.Core/Entities/Raid.cs
@@ -0,0 +1,13 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class Raid : PveInstance
+    {
+        public int BossesKilled { get; set; }
+        public ICollection<RaidCharacter> RaidCharacters { get; set; }
+    }
+}
