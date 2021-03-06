477a5030bb6928b3eb19c29259ca0f448d3e6340
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:32:47 +0100

    Create PveInstance base class

---
+++ src/Imi.Project.Api.Core/Entities/Base/PveInstance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Base
+{
+    public abstract class PveInstance : BaseEntity
+    {
+        public string InstanceName { get; set; }
+        public int AvailableBosses { get; set; }
+        public string Difficulty { get; set; }
+        public DateTime Date { get; set; }
+        public Guid LeaderId { get; set; }
+        public Character Leader { get; set; }
+        public ICollection<Character> CharactersParticipated { get; set; }
+    }
+}
