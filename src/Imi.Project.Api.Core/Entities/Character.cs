45e1490a28a4ca254fc3f63ac5e146cd3164b766
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:36:49 +0100

    Create Character entity

---
+++ src/Imi.Project.Api.Core/Entities/Character.cs
@@ -0,0 +1,24 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class Character : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Role { get; set; }
+        public int ItemLevel { get; set; }
+        public double ArenaRating { get; set; }
+        public double BattlegroundRating { get; set; }
+        public bool IsLeader { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<ArenaCharacter> ArenaCharacters { get; set; }
+        public ICollection<BattlegroundCharacter> BattlegroundCharacters { get; set; }
+        public ICollection<DungeonCharacter> DungeonCharacters { get; set; }
+        public ICollection<RaidCharacter> RaidCharacters { get; set; }
+    }
+}
