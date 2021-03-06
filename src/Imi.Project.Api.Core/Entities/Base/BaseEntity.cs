33a1fdbd849b4b284bde3cbb3ac117096d85804a
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:31:46 +0100

    Create Base entity

---
+++ src/Imi.Project.Api.Core/Entities/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Base
+{
+    public abstract class BaseEntity
+    {
+        Guid Id { get; set; }
+    }
+}
