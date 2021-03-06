e358a50804aaafab8a3fc199ebe0be281618569b
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-06 17:37:04 +0100

    Create Dungeon entity

---
+++ src/Imi.Project.Api.Core/Entities/Dungeon.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class Dungeon : PveInstance
+    {
+        public int? Timer { get; set; }
+        public bool Succes { get; set; }
+        public ICollection<DungeonCharacter> DungeonCharacters { get; set; }
+    }
+}
