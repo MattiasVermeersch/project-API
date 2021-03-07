96116eb570a36cd873574f32826623bca12839e8
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-07 13:49:34 +0100

    Create ArenaSeeder

---
+++ src/Imi.Project.Api.Infrastructure/Data/Seeding/ArenaSeeder.cs
@@ -0,0 +1,87 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Infrastructure.Data.Seeding
+{
+    public class ArenaSeeder
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Arena>().HasData(
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0001-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 12),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0010-000000000000"),
+                    IsWon = true
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0002-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 12),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0010-000000000000"),
+                    IsWon = true
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0003-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 12),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0010-000000000000"),
+                    IsWon = false
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0004-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 12),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0010-000000000000"),
+                    IsWon = true
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0005-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 13),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0018-000000000000"),
+                    IsWon = false
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0006-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 13),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0018-000000000000"),
+                    IsWon = false
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0007-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 13),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0018-000000000000"),
+                    IsWon = true
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0008-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 13),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0018-000000000000"),
+                    IsWon = false
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0009-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 13),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0018-000000000000"),
+                    IsWon = false
+                },
+                new Arena
+                {
+                    Id = Guid.Parse("00000000-0010-0000-000-000000000000"),
+                    Date = new DateTime(2021, 07, 13),
+                    LeaderId = Guid.Parse("00000000-0000-0000-0018-000000000000"),
+                    IsWon = false
+                }
+            );
+        }
+    }
+}
