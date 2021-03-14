cabd3732132e34e43abe0d489b4093246945c191
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-14 14:01:06 +0100

    Update PveInstance and Raid classes

--- src/Imi.Project.Api.Core/Entities/Base/PveInstance.cs
+++ src/Imi.Project.Api.Core/Entities/Base/PveInstance.cs
@@ -7,7 +7,6 @@ namespace Imi.Project.Api.Core.Entities.Base
     public abstract class PveInstance : BaseEntity
     {
         public string InstanceName { get; set; }
-        public int AvailableBosses { get; set; }
         public string Difficulty { get; set; }
         public DateTime Date { get; set; }
         public Guid LeaderId { get; set; }
