4852278bff824a298053693265028dbdd3b8993f
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 13:13:45 +0100

    Create UsersController with Get methods

---
+++ src/Imi.Project.Api/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Imi.Project.Api.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _userService.ListAllAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+
+            if(user == null)
+            {
+                return NotFound($"User with ID {id} could not be found.");
+            }
+
+            return Ok(user);
+        }
+    }
+}
