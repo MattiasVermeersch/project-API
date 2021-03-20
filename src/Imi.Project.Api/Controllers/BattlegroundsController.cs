7b6b5dd66633fc1503d1ce5fa42e9d85efdf4f2c
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 13:14:19 +0100

    Create BattlegroundsController with Get methods

---
+++ src/Imi.Project.Api/Controllers/BattlegroundsController.cs
@@ -0,0 +1,42 @@
+﻿using Imi.Project.Api.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BattlegroundsController : ControllerBase
+    {
+        private readonly IBattlegroundService _battlegroundService;
+        public BattlegroundsController(IBattlegroundService battlegroundService)
+        {
+            _battlegroundService = battlegroundService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var battleGrounds = await _battlegroundService.ListAllAsync();
+
+            return Ok(battleGrounds);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var battleGround = await _battlegroundService.GetByIdAsync(id);
+
+            if(battleGround == null)
+            {
+                return NotFound($"Battleground with ID {id} could not be found.");
+            }
+
+            return Ok(battleGround);
+        }
+    }
+}
