4db249a2ed108eedfc0f26e6ca80f4972207eff2
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 11:52:16 +0100

    Implement Clean architecture for RaidService

--- src/Imi.Project.Api.Core/Services/RaidService.cs
+++ src/Imi.Project.Api.Core/Services/RaidService.cs
@@ -1,32 +1,26 @@
 ﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 using Imi.Project.Api.Core.Dtos;
-using Imi.Project.Api.Core.Entities;
 using Imi.Project.Api.Core.Interfaces.Repositories;
 using Imi.Project.Api.Core.Interfaces.Services;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Services
 {
     public class RaidService : IRaidService
     {
-        private readonly IRepository<Raid> _raidRepository;
+        private readonly IRaidRepository _raidRepository;
         private readonly IMapper _mapper;
 
-        public RaidService(IRepository<Raid> raidRepo, IMapper mapper)
+        public RaidService(IRaidRepository raidRepo, IMapper mapper)
         {
             _raidRepository = raidRepo;
             _mapper = mapper;
         }
         public async Task<RaidResponseDto> GetByIdAsync(Guid id)
         {
-            var result = await _raidRepository.GetAllAsync()
-                .Include(a => a.RaidCharacters)
-                    .ThenInclude(ac => ac.Character)
-                .SingleOrDefaultAsync(a => a.Id.Equals(id));
+            var result = await _raidRepository.GetByIdAsync(id);
 
             var dto = _mapper.Map<RaidResponseDto>(result);
             return dto;
@@ -34,10 +28,7 @@ namespace Imi.Project.Api.Core.Services
 
         public async Task<IEnumerable<RaidResponseDto>> ListAllAsync()
         {
-            var result = await _raidRepository.GetAllAsync()
-                .Include(a => a.RaidCharacters)
-                    .ThenInclude(ac => ac.Character)
-                .ToListAsync();
+            var result = await _raidRepository.ListAllAsync();
 
             var dto = _mapper.Map<IEnumerable<RaidResponseDto>>(result);
             return dto;
