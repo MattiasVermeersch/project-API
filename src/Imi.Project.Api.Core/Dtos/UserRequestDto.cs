7fa7111f63eb5b0b87bbd56b6cfe2253f0398182
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 13:57:50 +0100

    Create RequestDto for User

---
+++ src/Imi.Project.Api.Core/Dtos/UserRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using Imi.Project.Api.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class UserRequestDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsRaidLeader { get; set; }
+        public bool IsWarlord { get; set; }
+    }
+}
