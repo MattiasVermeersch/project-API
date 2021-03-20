54c95b824cb9145032ad4dfbfa299296316d0e5c
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 11:51:56 +0100

    Implement Clean architecture for DungeonService

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/DungeonRepository.cs
@@ -0,0 +1,29 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Interfaces.Repositories;
+using Imi.Project.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class DungeonRepository : Repository<Dungeon>, IDungeonRepository
+    {
+        public DungeonRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+
+        public override IQueryable<Dungeon> GetAllAsync()
+        {
+            return _dbContext.Dungeons.AsNoTracking()
+                .Include(d => d.DungeonCharacters)
+                    .ThenInclude(dc => dc.Character);
+        }
+
+        public override async Task<Dungeon> GetByIdAsync(Guid id)
+        {
+            return await GetAllAsync().SingleOrDefaultAsync(d => d.Id.Equals(id));
+        }
+    }
+}
