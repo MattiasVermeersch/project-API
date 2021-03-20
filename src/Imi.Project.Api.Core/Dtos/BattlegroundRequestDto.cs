63af08d7da518fb54c427e6e9fe1bc56a0311b05
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 13:58:30 +0100

    Create RequestDto for Battleground

---
+++ src/Imi.Project.Api.Core/Dtos/BattlegroundRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class BattlegroundRequestDto : BaseDto
+    {
+        public string InstanceName { get; set; }
+        public bool IsWon { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
