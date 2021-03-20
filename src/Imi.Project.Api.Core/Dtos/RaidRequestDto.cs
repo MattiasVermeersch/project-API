517ef56cea3627aaab8e23570bdc732e00ac84b9
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 13:58:47 +0100

    Create RequestDto for Raid

---
+++ src/Imi.Project.Api.Core/Dtos/RaidRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using Imi.Project.Api.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class RaidRequestDto : BaseDto
+    {
+        public string InstanceName { get; set; }
+        public string Difficulty { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
