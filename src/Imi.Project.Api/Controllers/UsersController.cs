4b28befaf0d57ad585be893e60530ad81e87afaa
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 14:43:55 +0100

    Add CRUD methods to UsersController

--- src/Imi.Project.Api/Controllers/UsersController.cs
+++ src/Imi.Project.Api/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Imi.Project.Api.Core.Interfaces.Services;
+﻿using Imi.Project.Api.Core.Dtos;
+using Imi.Project.Api.Core.Interfaces.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -38,6 +39,45 @@ namespace Imi.Project.Api.Controllers
             }
 
             return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(UserRequestDto userRequestDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userResponseDto = await _userService.AddAsync(userRequestDto);
+            return CreatedAtAction(nameof(Get), new { id = userResponseDto.Id }, userResponseDto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(UserRequestDto userRequestDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userResponseDto = await _userService.UpdateAsync(userRequestDto);
+
+            return Ok(userResponseDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+
+            if(user == null)
+            {
+                return NotFound($"User with ID {id} could not be found.");
+            }
+
+            await _userService.DeleteAsync(id);
+            return Ok();
         }
     }
 }
