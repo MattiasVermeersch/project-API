6ef281b963bc6fd2c4df8d164fab430292b28805
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 13:14:37 +0100

    Create RaidsController with Get methods

---
+++ src/Imi.Project.Api/Controllers/RaidsController.cs
@@ -0,0 +1,43 @@
+﻿using Imi.Project.Api.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RaidsController : ControllerBase
+    {
+        private readonly IRaidService _raidService;
+        
+        public RaidsController(IRaidService raidService)
+        {
+            _raidService = raidService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var raids = await _raidService.ListAllAsync();
+
+            return Ok(raids);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var raid = await _raidService.GetByIdAsync(id);
+
+            if(raid == null)
+            {
+                return NotFound($"Raid with ID {id} could not be found.");
+            }
+
+            return Ok(raid);
+        }
+    }
+}
