2e3abc68c6d5733eff701116789ac902fa5a357b
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 11:51:25 +0100

    Implement Clean architecture for BattlegroundService

--- src/Imi.Project.Api.Core/Services/BattlegroundService.cs
+++ src/Imi.Project.Api.Core/Services/BattlegroundService.cs
@@ -1,22 +1,19 @@
 ﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 using Imi.Project.Api.Core.Dtos;
-using Imi.Project.Api.Core.Entities;
 using Imi.Project.Api.Core.Interfaces.Repositories;
 using Imi.Project.Api.Core.Interfaces.Services;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Services
 {
     public class BattlegroundService : IBattlegroundService
     {
-        private readonly IRepository<Battleground> _battlegroundRepository;
+        private readonly IBattlegroundRepository _battlegroundRepository;
         private readonly IMapper _mapper;
 
-        public BattlegroundService(IRepository<Battleground> battlegroundRepo, IMapper mapper)
+        public BattlegroundService(IBattlegroundRepository battlegroundRepo, IMapper mapper)
         {
             _battlegroundRepository = battlegroundRepo;
             _mapper = mapper;
@@ -24,10 +21,7 @@ namespace Imi.Project.Api.Core.Services
 
         public async Task<BattlegroundResponseDto> GetByIdAsync(Guid id)
         {
-            var result = await _battlegroundRepository.GetAllAsync()
-                .Include(b => b.BattlegroundCharacters)
-                    .ThenInclude(bc => bc.Character)
-                .SingleOrDefaultAsync(b => b.Id.Equals(id));
+            var result = await _battlegroundRepository.GetByIdAsync(id);
 
             var dto = _mapper.Map<BattlegroundResponseDto>(result);
             return dto;
@@ -35,10 +29,7 @@ namespace Imi.Project.Api.Core.Services
 
         public async Task<IEnumerable<BattlegroundResponseDto>> ListAllAsync()
         {
-            var result = await _battlegroundRepository.GetAllAsync()
-                .Include(b => b.BattlegroundCharacters)
-                    .ThenInclude(bc => bc.Character)
-                .ToListAsync();
+            var result = await _battlegroundRepository.ListAllAsync();
 
             var dto = _mapper.Map<IEnumerable<BattlegroundResponseDto>>(result);
             return dto;
