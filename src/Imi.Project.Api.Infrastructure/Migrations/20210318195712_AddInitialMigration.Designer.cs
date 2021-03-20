54f5ce8bce9e9d252d87f64c0105fe418887bb06
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 10:24:23 +0100

    Add new InitialMigration

--- src/Imi.Project.Api.Infrastructure/Migrations/20210318195712_AddInitialMigration.Designer.cs
+++ src/Imi.Project.Api.Infrastructure/Migrations/20210318195712_AddInitialMigration.Designer.cs
@@ -4,14 +4,16 @@ using Imi.Project.Api.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Imi.Project.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210318195712_AddInitialMigration")]
+    partial class AddInitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
