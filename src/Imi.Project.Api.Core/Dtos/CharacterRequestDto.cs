366347985b3d3029156fa1eb994ca6fe924d16d5
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 13:58:09 +0100

    Create RequestDto for Character

---
+++ src/Imi.Project.Api.Core/Dtos/CharacterRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using Imi.Project.Api.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class CharacterRequestDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Role { get; set; }
+        public int Level { get; set; }
+        public int ItemLevel { get; set; }
+        public double ArenaRating { get; set; }
+        public double BattlegroundRating { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
