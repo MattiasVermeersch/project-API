4a3562f975f4fc8b66a00ff20cd7c545fde1f394
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-20 11:52:46 +0100

    Implement Clean architecture for CharacterService

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/CharacterRepository.cs
@@ -0,0 +1,36 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Interfaces.Repositories;
+using Imi.Project.Api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class CharacterRepository : Repository<Character>, ICharacterRepository
+    {
+        public CharacterRepository(ApplicationDbContext dbContext) : base(dbContext) { }
+
+        public override IQueryable<Character> GetAllAsync()
+        {
+            return _dbContext.Characters.AsNoTracking()
+                .Include(c => c.ArenaCharacters)
+                    .ThenInclude(ac => ac.Arena)
+                .Include(c => c.BattlegroundCharacters)
+                    .ThenInclude(bc => bc.Battleground)
+                .Include(c => c.DungeonCharacters)
+                    .ThenInclude(dc => dc.Dungeon)
+                .Include(c => c.RaidCharacters)
+                    .ThenInclude(rc => rc.Raid)
+                .Include(c => c.User);
+        }
+
+        public override async Task<Character> GetByIdAsync(Guid id)
+        {
+            return await GetAllAsync().SingleOrDefaultAsync(c => c.Id.Equals(id));
+        }
+    }
+}
