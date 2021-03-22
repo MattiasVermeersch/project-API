e807798895532c37035b9c537f39459f2d6808e5
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-22 19:08:12 +0100

    Implement new method from interface

--- src/Imi.Project.Api.Infrastructure/Repositories/ArenaRepository.cs
+++ src/Imi.Project.Api.Infrastructure/Repositories/ArenaRepository.cs
@@ -25,5 +25,14 @@ namespace Imi.Project.Api.Infrastructure.Repositories
         {
             return await GetAllAsync().SingleOrDefaultAsync(a => a.Id.Equals(id));
         }
+
+        public async Task<IEnumerable<Arena>> GetByCharacterId(Guid id)
+        {
+            return await GetAllAsync()
+                .Where(a => a.ArenaCharacters.Any(ac => ac.CharacterId.Equals(id)))
+                .ToListAsync();
+        }
+
+        
     }
 }
