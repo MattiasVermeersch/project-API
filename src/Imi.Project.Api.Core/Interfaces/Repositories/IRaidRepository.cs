6142ebb4c69f01a17b938cf0854a3a43d685a6b5
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-22 19:11:19 +0100

    Add GetByCharacterId method to RaidRepository interface

--- src/Imi.Project.Api.Core/Interfaces/Repositories/IRaidRepository.cs
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/IRaidRepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Repositories
 {
     public interface IRaidRepository : IRepository<Raid>
     {
+        Task<IEnumerable<Raid>> GetByCharacterId(Guid id);
     }
 }
