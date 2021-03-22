58fa41724195a2707679d3e3452cef0556d08d85
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-22 19:07:53 +0100

    Add GetByCharacterId method to ArenaRepository interface

--- src/Imi.Project.Api.Core/Interfaces/Repositories/IArenaRepository.cs
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/IArenaRepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Repositories
 {
     public interface IArenaRepository : IRepository<Arena>
     {
+        Task<IEnumerable<Arena>> GetByCharacterId(Guid id);
     }
 }
