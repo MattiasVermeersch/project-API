82c4a75505528e387abadd4c0902cc97221222c4
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-22 19:09:25 +0100

    Add GetByCharacterId method to DungeonRepository interface

--- src/Imi.Project.Api.Core/Interfaces/Repositories/IDungeonRepository.cs
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/IDungeonRepository.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Repositories
 {
     public interface IDungeonRepository : IRepository<Dungeon>
     {
+        Task<IEnumerable<Dungeon>> GetByCharacterId(Guid id);
     }
 }
