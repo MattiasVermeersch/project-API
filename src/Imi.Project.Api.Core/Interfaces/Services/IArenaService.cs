a9a5cb014e4c2026c7aac82508b24daa63fc43fa
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-03-25 22:03:53 +0100

    Add Add, Update and Delete methods to IArenaService

--- src/Imi.Project.Api.Core/Interfaces/Services/IArenaService.cs
+++ src/Imi.Project.Api.Core/Interfaces/Services/IArenaService.cs
@@ -11,5 +11,8 @@ namespace Imi.Project.Api.Core.Interfaces.Services
         Task<IEnumerable<ArenaResponseDto>> ListAllAsync();
         Task<ArenaResponseDto> GetByIdAsync(Guid id);
         Task<IEnumerable<ArenaResponseDto>> GetByCharacterId(Guid id);
+        Task<ArenaResponseDto> AddAsync(ArenaRequestDto arenaRequestDto);
+        Task<ArenaResponseDto> UpdateAsync(ArenaRequestDto arenaRequestDto);
+        Task DeleteAsync(Guid id);
     }
 }
