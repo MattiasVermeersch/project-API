d9d447ae19505451bf2e0a0703010511b6a5f4a6
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-04-01 21:11:37 +0200

    Create SimpleCharacterResponseDto

---
+++ src/Imi.Project.Api.Core/Dtos/SimpleCharacterResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using Imi.Project.Api.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class SimpleCharacterResponseDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Role { get; set; }
+        public int ItemLevel { get; set; }
+    }
+}
