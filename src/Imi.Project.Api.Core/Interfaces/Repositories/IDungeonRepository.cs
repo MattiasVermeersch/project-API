60247ff6778808823835a96046930b7ad978f211
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-04-01 22:07:23 +0200

    Add DeleteCharacter method to DungeonRepository interface

--- src/Imi.Project.Api.Core/Interfaces/Repositories/IDungeonRepository.cs
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/IDungeonRepository.cs
@@ -10,5 +10,6 @@ namespace Imi.Project.Api.Core.Interfaces.Repositories
     {
         Task<IEnumerable<Dungeon>> GetByCharacterId(Guid id);
         Task<Dungeon> AddCharacterAsync(Guid id, Character character);
+        Task<Dungeon> DeleteCharacterAsync(Guid id, Character character);
     }
 }
