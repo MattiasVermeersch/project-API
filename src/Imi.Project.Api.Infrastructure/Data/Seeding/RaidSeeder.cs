48226cc02cf16d12b72daba012b2c85f13756f27
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-04-10 11:44:43 +0200

    Add Raid data to Seeder

--- src/Imi.Project.Api.Infrastructure/Data/Seeding/RaidSeeder.cs
+++ src/Imi.Project.Api.Infrastructure/Data/Seeding/RaidSeeder.cs
@@ -37,6 +37,24 @@ namespace Imi.Project.Api.Infrastructure.Data.Seeding
                     Difficulty = "Heroic",
                     Date = new DateTime(2021, 03, 10),
                     BossesKilled = 10
+                },
+                new Raid
+                {
+                    Id = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    InstanceName = "Caste Nathria",
+                    AvailableBosses = 10,
+                    Difficulty = "Heroic",
+                    Date = new DateTime(2021, 03, 17),
+                    BossesKilled = 4
+                },
+                new Raid
+                {
+                    Id = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    InstanceName = "Caste Nathria",
+                    AvailableBosses = 10,
+                    Difficulty = "Heroic",
+                    Date = new DateTime(2021, 03, 24),
+                    BossesKilled = 4
                 }
             );
         }
