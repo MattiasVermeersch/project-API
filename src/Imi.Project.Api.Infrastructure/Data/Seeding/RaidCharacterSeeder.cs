2dbbe67c218c295f673adc7b4bc1d359c0cfa0a8
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-04-10 11:45:56 +0200

    Add cross table data for RaidCharacterSeeder

--- src/Imi.Project.Api.Infrastructure/Data/Seeding/RaidCharacterSeeder.cs
+++ src/Imi.Project.Api.Infrastructure/Data/Seeding/RaidCharacterSeeder.cs
@@ -195,6 +195,130 @@ namespace Imi.Project.Api.Infrastructure.Data.Seeding
                     //Dps 5
                     RaidId = Guid.Parse("30000000-0000-0000-0000-000000000000"),
                     CharacterId = Guid.Parse("00000000-0000-0000-0024-000000000000")
+                },
+
+                //fourth raid
+                new RaidCharacter
+                {
+                    //Tank 1
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0001-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Tank 2
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0025-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Healer 1
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0004-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Healer 2
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0016-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Healer 3
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0018-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 1
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0002-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 2
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0005-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 3
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0008-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 4
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0012-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 5
+                    RaidId = Guid.Parse("40000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0024-000000000000")
+                },
+
+                //fifth raid
+                new RaidCharacter
+                {
+                    //Tank 1
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0009-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Tank 2
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0025-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Healer 1
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0004-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Healer 2
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0016-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Healer 3
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0019-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 1
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0002-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 2
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0005-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 3
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0008-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 4
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0012-000000000000")
+                },
+                new RaidCharacter
+                {
+                    //Dps 5
+                    RaidId = Guid.Parse("50000000-0000-0000-0000-000000000000"),
+                    CharacterId = Guid.Parse("00000000-0000-0000-0024-000000000000")
                 }
             );
         }
