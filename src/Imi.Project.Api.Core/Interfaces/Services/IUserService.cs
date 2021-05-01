fca706960408928ded3c7e14ce68886ed86e765f
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-02 00:11:00 +0200

    Update UserService interface with Identity

--- src/Imi.Project.Api.Core/Interfaces/Services/IUserService.cs
+++ src/Imi.Project.Api.Core/Interfaces/Services/IUserService.cs
@@ -9,9 +9,9 @@ namespace Imi.Project.Api.Core.Interfaces.Services
     public interface IUserService
     {
         Task<IEnumerable<UserResponseDto>> ListAllAsync();
-        Task<UserResponseDto> GetByIdAsync(Guid id);
+        Task<UserResponseDto> GetByIdAsync(string id);
         Task<UserResponseDto> AddAsync(UserRequestDto userRequestDto);
         Task<UserResponseDto> UpdateAsync(UserRequestDto userRequestDto);
-        Task DeleteAsync(Guid id);
+        Task DeleteAsync(string id);
     }
 }
