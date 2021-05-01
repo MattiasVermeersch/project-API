f962193612296c59911e2007834b203aac318a3e
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-02 00:02:18 +0200

    Update interface repository to inherit from base

--- src/Imi.Project.Api.Core/Interfaces/Repositories/IRepository.cs
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/IRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Imi.Project.Api.Core.Entities.Base;
+using Imi.Project.Api.Core.Interfaces.Repositories.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +9,7 @@ using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Repositories
 {
-    public interface IRepository<T> where T : BaseEntity
-    {
-        Task<T> GetByIdAsync(Guid id);
-        Task<T> GetByIdAsync(Guid id, string[] includes);
-        IQueryable<T> GetAllAsync();
-        Task<IEnumerable<T>> ListAllAsync();
-        IQueryable<T> GetFiltered(Expression<Func<T, bool>> predicate);
-        Task<IEnumerable<T>> ListFiltered(Expression<Func<T, bool>> predicate);
-        Task<T> AddAsync(T entity);
-        Task<T> UpdateAsync(T entity);
-        Task<T> DeleteAsync(T entity);
-        Task<T> DeleteAsync(Guid id);
-    }
+    public interface IRepository<T> : IBaseRepository<T, Guid>
+        where T : BaseEntity
+    {    }
 }
