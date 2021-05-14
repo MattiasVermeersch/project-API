46583138b023ba6672ea99ea99dcd6b31d19284f
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-14 11:44:05 +0200

    Update AddAsync method from URepository

--- src/Imi.Project.Api.Infrastructure/Repositories/UserRepository.cs
+++ src/Imi.Project.Api.Infrastructure/Repositories/UserRepository.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,13 +18,40 @@ namespace Imi.Project.Api.Infrastructure.Repositories
 
         public override IQueryable<User> GetAllAsync()
         {
-            return _userManager.Users.AsNoTracking()
+            return _userManager.Users
                 .Include(u => u.Characters);
         }
 
         public override async Task<User> GetByIdAsync(string id)
         {
             return await GetAllAsync().SingleOrDefaultAsync(u => u.Id.Equals(id));
+        }
+
+        public override async Task<User> AddAsync(User user)
+        {
+            var password = user.Password;
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if(!result.Succeeded) { 
+                user.Error = result;
+                await _userManager.DeleteAsync(user);
+                return user; 
+            }
+
+            
+            User newUser = await _userManager.FindByEmailAsync(user.Email);
+            await _userManager.AddClaimAsync(newUser,
+                new Claim("registration-date", DateTime.Now.ToString("yyyy-MM-dd")));
+
+            string hasCharacters;
+            if (newUser.Characters == null) hasCharacters = "false";
+            else hasCharacters = "true";
+
+            await _userManager.AddClaimAsync(newUser,
+                new Claim("has-characters", hasCharacters));
+
+            return newUser;
         }
     }
 }
