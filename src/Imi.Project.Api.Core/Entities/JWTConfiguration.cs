f97e96921eb764c1adebad7f14713e370dc00041
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-14 16:09:54 +0200

    Create JWTConfiguration class

---
+++ src/Imi.Project.Api.Core/Entities/JWTConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities
+{
+    public class JWTConfiguration
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SigningKey { get; set; }
+        public int TokenExpirationDays { get; set; }
+    }
+}
