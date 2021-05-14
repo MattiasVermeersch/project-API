02410b1f14a7d409c306f1056be6b0478789fe38
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-14 11:46:05 +0200

    Create dto to register a user

---
+++ src/Imi.Project.Api.Core/Dtos/RegisterUserRequestDto.cs
@@ -0,0 +1,43 @@
+﻿using Imi.Project.Api.Core.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class RegisterUserRequestDto
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 7)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Text)]
+        public string City { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+    }
+}
