4efbc837316117abf08aaed4886a99307edc655c
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-14 16:09:34 +0200

    Create Login dtos

---
+++ src/Imi.Project.Api.Core/Dtos/LoginUserResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Dtos
+{
+    public class LoginUserResponseDto
+    {
+        public string Token { get; set; }
+        public bool SignInSucceeded { get; set; } = true;
+    }
+}
