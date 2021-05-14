0b1d0ec393884762e2dff6b3fb98243ddcaf4217
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-14 11:44:34 +0200

    Update interface URepository

--- src/Imi.Project.Api.Core/Interfaces/Repositories/IUserRepository.cs
+++ src/Imi.Project.Api.Core/Interfaces/Repositories/IUserRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Imi.Project.Api.Core.Entities;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Imi.Project.Api.Core.Interfaces.Repositories
 {
