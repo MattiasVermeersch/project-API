34efc3171b4e7bf5d43d73cb23659333ceb28a95
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-05-16 20:32:07 +0200

    Update Startup to use tokens in Swagger

--- src/Imi.Project.Api/Startup.cs
+++ src/Imi.Project.Api/Startup.cs
@@ -16,9 +16,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -82,7 +86,59 @@ namespace Imi.Project.Api
                 };
             });
 
-            services.AddSwaggerGen();
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("OnlyUsersWithCharacters", policy =>
+                {
+                    policy.RequireClaim("has-characters", "true");
+                });
+                options.AddPolicy("RegisteredForAWeek", policy =>
+                {
+                    policy.RequireAssertion(context =>
+                    {
+                        var registrationClaimValue = context.User.Claims.SingleOrDefault(claim => claim.Type == "registration-date")?.Value;
+                        if (DateTime.TryParseExact(registrationClaimValue, "yyyy-MM-dd",
+                            CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal,
+                            out var registrationTime))
+                        {
+                            return registrationTime.AddDays(7) < DateTime.UtcNow;
+                        }
+                        return false;
+                    });
+                });
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API V1", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. <br\ >
+                                    Enter 'Bearer' and then your token in the text input below. <br\ >
+                                    Example: 'Bearer abcdef123456789'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -100,8 +156,6 @@ namespace Imi.Project.Api
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
                 c.RoutePrefix = string.Empty;
             });
-            
-
 
             app.UseHttpsRedirection();
 
