a82c0a0bae42252ac32496dec5a6ee643032bc63
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-06-07 23:46:04 +0200

    Add Vue.js file for layout page

--- src/Imi.Project.Vue/wwwroot/js/site.js
+++ src/Imi.Project.Vue/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var role = sessionStorage.getItem("userRole");
+var token = sessionStorage.getItem("token");
+
+var app = new Vue({
+    el: "#navbar",
+    data: {
+        role: role,
+        token: token,
+    },
+    created: function () {
+
+    },
+    methods: {
+        logout: function () {
+            sessionStorage.clear();
+            window.location.href= 'Login';
+        },
+        manageAccount: function () {
+            window.location.href = 'Manage';
+        }
+    },
+});
