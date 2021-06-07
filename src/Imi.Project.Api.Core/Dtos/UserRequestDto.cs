9725c87605609f0495132ce5e20ddc820fa1f9b6
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-06-07 23:23:53 +0200

    Update user request dto for role

--- src/Imi.Project.Api.Core/Dtos/UserRequestDto.cs
+++ src/Imi.Project.Api.Core/Dtos/UserRequestDto.cs
@@ -8,11 +8,12 @@ namespace Imi.Project.Api.Core.Dtos
     public class UserRequestDto : BaseIdentityDto
     {
         public DateTime BirthDate { get; set; }
-        public string UserName { get; set; }
         public string Email { get; set; }
+        public string UserName { get; set; }
         public string Address { get; set; }
         public string FullName { get; set; }
         public string Password { get; set; }
         public string City { get; set; }
+        public string Role { get; set; }
     }
 }
