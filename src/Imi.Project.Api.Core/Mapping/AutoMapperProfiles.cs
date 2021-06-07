fab60d28969ea4cbea7d8a7691a42f41dbcc423d
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-06-07 23:25:12 +0200

    Update mapping for User and simple character response

--- src/Imi.Project.Api.Core/Mapping/AutoMapperProfiles.cs
+++ src/Imi.Project.Api.Core/Mapping/AutoMapperProfiles.cs
@@ -13,7 +13,17 @@ namespace Imi.Project.Api.Core.Mapping
         public AutoMapperProfiles()
         {
             //Mapping for User entity
-            CreateMap<User, UserResponseDto>();
+            CreateMap<User, UserResponseDto>()
+                .ForMember(dest => dest.Characters,
+                    opt => opt.MapFrom(src => src.Characters
+                    .Select(uc => new SimpleCharacterResponseDto
+                    {
+                        Id = uc.Id,
+                        Name = uc.Name,
+                        Class = uc.Class,
+                        Role = uc.Role,
+                        ItemLevel = uc.ItemLevel
+                    })));
             CreateMap<RegisterUserRequestDto, User>();
             CreateMap<UserRequestDto, User>();
 
@@ -26,7 +36,9 @@ namespace Imi.Project.Api.Core.Mapping
                 .ForMember(dest => dest.DungeonCount,
                     opt => opt.MapFrom(src => src.DungeonCharacters.Count()))
                 .ForMember(dest => dest.RaidCount,
-                    opt => opt.MapFrom(src => src.RaidCharacters.Count()));
+                    opt => opt.MapFrom(src => src.RaidCharacters.Count()))
+                .ForMember(dest => dest.UserName,
+                    opt => opt.MapFrom(src => src.User.FullName));
             CreateMap<Character, SimpleCharacterResponseDto>();
             CreateMap<CharacterRequestDto, Character>();
 
@@ -42,7 +54,8 @@ namespace Imi.Project.Api.Core.Mapping
                             Name = ac.Character.Name,
                             Class = ac.Character.Class,
                             Role = ac.Character.Role,
-                            ItemLevel = ac.Character.ItemLevel
+                            ItemLevel = ac.Character.ItemLevel,
+                            UserId = ac.Character.UserId
                         })));
             CreateMap<ArenaRequestDto, Arena>();
 
@@ -58,7 +71,8 @@ namespace Imi.Project.Api.Core.Mapping
                             Name = bc.Character.Name,
                             Class = bc.Character.Class,
                             Role = bc.Character.Role,
-                            ItemLevel = bc.Character.ItemLevel
+                            ItemLevel = bc.Character.ItemLevel,
+                            UserId = bc.Character.UserId
                         })));
             CreateMap<BattlegroundRequestDto, Battleground>();
 
@@ -68,11 +82,12 @@ namespace Imi.Project.Api.Core.Mapping
                     opt => opt.MapFrom(src => src.DungeonCharacters
                         .Select(dc => new SimpleCharacterResponseDto
                         {
-                            Id = dc.CharacterId,
+                            Id = dc.Character.Id,
                             Name = dc.Character.Name,
                             Class = dc.Character.Class,
                             Role = dc.Character.Role,
-                            ItemLevel = dc.Character.ItemLevel
+                            ItemLevel = dc.Character.ItemLevel,
+                            UserId = dc.Character.UserId
                         })));
             CreateMap<DungeonRequestDto, Dungeon>();
 
@@ -86,7 +101,8 @@ namespace Imi.Project.Api.Core.Mapping
                             Name = rc.Character.Name,
                             Class = rc.Character.Class,
                             Role = rc.Character.Role,
-                            ItemLevel = rc.Character.ItemLevel
+                            ItemLevel = rc.Character.ItemLevel,
+                            UserId = rc.Character.UserId
                         })));
             CreateMap<RaidRequestDto, Raid>();
         }
